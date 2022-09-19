f4b73da9433787ac2bcf1a508f4f3acce85de496
lynnlo <tharlynnhtet.ong@gmail.com>
2022-09-18 20:07:54 -0700

    Update Style

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, PoiretOne_400Regular } from "@expo-google-fonts/dev";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let [fontsLoaded] = useFonts({
+      PoiretOne_400Regular
+    })
+    
+    return (
+      <View style={styles.view}>
+        <StatusBar style="auto" />
+        <Text style={styles.textMain}> Hello World! </Text>
+      </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0D1B2A',
+    color: '#E0E1DD',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textMain: {
+    color: '#E0E1DD',
+    fontSize: '4rem',
+    fontFamily: 'PoiretOne_400Regular'
+  }
 });
